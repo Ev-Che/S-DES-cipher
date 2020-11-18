997b03a261020c54e1c6afba027834bf5079779d
Yauheni <e.subbota@syberry.com>
2020-11-18 10:34:09 +0300

    refactoring part of the code

--- tools/key_generator.py
+++ tools/key_generator.py
@@ -1,40 +1,42 @@
 from tools import tools
-import logging
+
+GEN_SCHEMA_8 = [6, 3, 7, 4, 8, 5, 10, 9]
+GEN_SCHEMA_10 = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
+
+BIT_ORDER = 10
 
 
 class KeyGenerator:
-    """Класс для вычисления ключей шифрования."""
+    """Class that allows calculating key for encrypting"""
 
-    def __init__(self):
-        self.bit_key = None
-        self.k1 = self.k2 = None
+    def __init__(self, input_key):
+        self.bit_key = self.bit_key_compile(input_key)
+        self.k1 = None
+        self.k2 = None
 
-    def __bit_key_compile(self, input_key):
-        """Формирование 10-битной двоичной последовательности из входного ключа."""
+    @staticmethod
+    def bit_key_compile(input_key: int) -> list:
+        """This function creates 10-bit sequence from user key"""
         bin_key = list(bin(input_key))[2:]
-        self.bit_key = ['0'] * (10 - len(bin_key)) + bin_key
+        return ['0'] * (BIT_ORDER - len(bin_key)) + bin_key
 
-    def __p_10(self):
-        schema = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
-        self.bit_key = [self.bit_key[item - 1] for item in schema]
+    @staticmethod
+    def replacement_elements(gen_schema: list, bit_key: list) -> list:
+        """This function replacements order according to the schema"""
+        return [bit_key[item - 1] for item in gen_schema]
 
-    def __cyclic_left_shift(self, times=1):
-        """Циклический сдвиг на times позиций влево и соединение левой и провой частей."""
+    def __cyclic_left_shift(self, times: int) -> None:
+        """This function shifts elements with times period and concatenates left and right part in one"""
         left_part, right_part = tools.division_into_two_parts(self.bit_key)
-        self.bit_key = (tools.left_shift(left_part, times=times) +
-                        tools.left_shift(right_part, times=times))
+        self.bit_key = tools.left_shift(left_part, times=times) + tools.left_shift(right_part, times=times)
 
-    def __p_8(self):
-        schema = [6, 3, 7, 4, 8, 5, 10, 9]
-        return [self.bit_key[item - 1] for item in schema]
+    def key_generate(self):
+        self.bit_key = self.replacement_elements(GEN_SCHEMA_10, self.bit_key)
 
-    def key_generate(self, input_key):
-        """Генерация ключей."""
-        self.__bit_key_compile(input_key)
-        self.__p_10()
-        self.__cyclic_left_shift()
-        self.k1 = self.__p_8()
+        self.__cyclic_left_shift(times=1)
+
+        self.k1 = self.replacement_elements(GEN_SCHEMA_8, self.bit_key)
+
         self.__cyclic_left_shift(times=2)
-        self.k2 = self.__p_8()
-        logging.info('KEYS:\n\tK1 = {k1}\n\tK2 = {k2}'.format(k1=tools.format_to_str(self.k1),
-                                                              k2=tools.format_to_str(self.k2)))
+
+        self.k2 = self.replacement_elements(GEN_SCHEMA_8, self.bit_key)
